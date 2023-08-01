44e6dac5cd2904707c2f13924fa432662f1d20c0
ulukbekski <ulukbekski@gmail.com>
2023-08-01 18:47:29 +0600

    начало проекта

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles/global.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=" text-xl px-10  text-[red] font-bold align-middle justify-center mt-[82px] blueBorder">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
